789ae635d455f036614f268d53c40ef863c3ee15
CHZelaya <chzelaya1201@gmail.com>
2025-05-15 14:58:12 -0600

    [cors] Added cors allowed domains and methods

---
+++ src/main/java/beto/projects/ipdbuddyapiv2/config/CorsConfig.java
@@ -0,0 +1,51 @@
+package beto.projects.ipdbuddyapiv2.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // Allowed origins
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:3003",
+                "https://teal-creponne-2f943b.netlify.app/"
+        ));
+
+        //Allowed methods
+        corsConfiguration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        //Allowed headers
+        corsConfiguration.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type"
+        ));
+
+        //Allowed credentials
+        corsConfiguration.setAllowCredentials(true);
+
+        //Register config for all paths
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+
+}
--- src/main/java/beto/projects/ipdbuddyapiv2/config/SecurityConfig.java
+++ src/main/java/beto/projects/ipdbuddyapiv2/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -30,8 +31,9 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(authManager -> {
                     authManager.requestMatchers(HttpMethod.POST, WHITELISTED_API_ENDPOINTS)
                             .permitAll()
