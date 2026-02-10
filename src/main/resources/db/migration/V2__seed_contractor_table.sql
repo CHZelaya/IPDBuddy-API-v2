d25fad111710fa0bdbf9cb0fa91b0a93bbaffe7a
CHZelaya <chzelaya1201@gmail.com>
2026-02-10 12:47:08 -0700

    [fix/feature] - Added a migration to seed a demo user - Moved migrations into appropriate location expected by flyway.

---
+++ src/main/resources/db/migration/V2__seed_contractor_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO contractor (email, first_name, last_name, phone_number, savings_rate, tax_rate)
+select
+    'ipdbuddy@demo.com',
+    'demo',
+    'user',
+    403-123-4567,
+    2.0,
+    2.0
+    WHERE NOT EXISTS(
+    SELECT 1 FROM contractor WHERE email = 'ipdbuddy@demo.com'
+);
+
+
+
